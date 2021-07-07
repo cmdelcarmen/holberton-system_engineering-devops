11c96bf60bc3af0c69822b9dab1693add233eb8b
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-07 20:21:09 +0000

    update

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,9 +1,9 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 
 #Create a Ruby script that takes one argument and passes it to a regular
 #expression matching method
 
 ARGV.each do |input|
-	h = input.scan(/h[0-9a-zA-Z_]*\W*n/).join
+	h = input.scan(/h.[0-9a-zA-Z_]*\W*n/).join
 	puts h
 end
