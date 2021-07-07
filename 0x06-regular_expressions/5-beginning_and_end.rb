01f2f62c6511243883ff87cd3bc0934ca027a6d5
cmdelcarmen <delcarmencaroline@gmail.com>
2021-07-07 20:04:12 +0000

    ans for num 5

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+#Create a Ruby script that takes one argument and passes it to a regular
+#expression matching method
+
+ARGV.each do |input|
+	h = input.scan(/h[0-9a-zA-Z_]*n/).join
+	puts h
+end
