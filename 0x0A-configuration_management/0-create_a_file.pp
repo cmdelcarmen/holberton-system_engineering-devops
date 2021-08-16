5d3768362412f24d24324aaecac161dbba38f5e5
cmdelcarmen <delcarmencaroline@gmail.com>
2021-08-16 20:06:10 +0000

    fixed typo

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -5,8 +5,8 @@
 # File group is www-data
 # File contains I love Puppet
 
-file { 'holberton' :
-path    => '/tmp/Holberton',
+file { 'holberton':
+path    => '/tmp/holberton',
 mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
