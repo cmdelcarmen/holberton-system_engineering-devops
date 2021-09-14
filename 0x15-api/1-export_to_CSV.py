2a667382a9be371058af3ff999986217cf856bda
Caroline Del Carmen <delcarmencaroline@gmail.com>
2021-09-14 03:10:50 +0000

    Using what you did in the task #0, extend your Python script to export data in the CSV format.

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+    Write a Python script that, using this REST API, for a given employee ID,
+    returns information about his/her TODO list progress.
+'''
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    # gettign employee name
+    emp_ID = argv[1]
+    response = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(emp_ID))
+    emp = response.json()['name']
+
+    # getting a list of tasks completed
+    response = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+                emp_ID))
+    emp_tasks = response.json()
+
+    with open("{}.csv".format(emp_ID), 'w') as cvs_file:
+        for value in emp_tasks:
+            cvs_file.write(
+                    "{}, {}, {}, {}\n".format(
+                        emp_ID, emp, value['completed'], value['title']))
